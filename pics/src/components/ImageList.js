1d113a6cddf730d83a2520d7863b31497f2f3ec0
sbrarCPTS <sbrar@crosslinktax.com>
2019-03-07 15:11:51 -0800

    Adding Image style to page

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
-        return <img key={image.id}  src={image.urls.regular} alt={image.description}></img>
+        return <ImageCard key={image.id} image={image}/>;
     });
 
     return(
-        <div>{images}</div>
+        <div className = "image-list">{images}</div>
     );
 }
 
